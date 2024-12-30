e68ab4dd02b21eff074e82857b33a6fecc8ba9f0
anais <mouelhi.chacha@gmail.com>
2024-12-30 23:55:43 +0100

    Votre message de commit

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -10,7 +10,7 @@ export default async function Dashboard() {
         <div className="flex flex-col h-[calc(100vh-6rem)] md:h-[calc(100vh-9rem)] lg:flex-row p-8">
           <div className="flex-1 flex items-center justify-center flex-col gap-8 text-red-500 font-bold">
             <h1 className="text-5xl text-center uppercase p-4 md:p-10 md:text-6xl xl:text-7xl">
-              Pret 
+              Prete
             </h1>
           </div>
      
